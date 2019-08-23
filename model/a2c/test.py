c7d4b0503b0de2149209e273ddaffad5d77f5fc3
x2ever <x2ever@naver.com>
2019-08-23 23:02:28 +0900

    Change Env settings

--- model/a2c/test.py
+++ model/a2c/test.py
@@ -15,9 +15,9 @@ from model.setting import people
 model_directory = os.path.dirname(os.path.realpath(__file__)) + "/a2c-models/"
 
 n_cpu = 1
-env = SubprocVecEnv([lambda: gym.make('gym_building:building-v0', people=people, num_of_lift=3, height_of_building=5) for i in range(1)])
+env = SubprocVecEnv([lambda: gym.make('gym_building:building-v0', people=people, num_of_lift=2, height_of_building=5) for i in range(1)])
 
-model = A2C.load(model_directory + "a2c-model_660000.pkl")
+model = A2C.load(model_directory + "a2c-model_5000.pkl")
 
 obs = env.reset()
 
